49c9dff5a3913a1241a70bafa60462307f1a65af
Matt Olson <matt@mattolson.com>
2012-09-27 14:07:41 -0700

    Add ruby solution to problem #1

---
+++ problems/1/ruby/solution.rb
@@ -0,0 +1,35 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+#
+
+def first_attempt
+  sum = 0
+  (1...1000).each do |val|
+    sum += val if val % 3 == 0 || val % 5 == 0
+  end
+  puts sum
+end
+
+def optimized
+  # This is a sum of two arithmetic series, the multiples of 3 and the multiples of 5
+  # Figure out how many multiples of three there are and where it ends, same for five,
+  # and add them together, but we must account for double counting of multiples of 15.
+  #
+  # sum of arithmetic series = n/2 * (a + z) where n is the number of terms,
+  # a is the first term, and z is the last term
+
+  multiples_of_three = 333 * (3 + 999) / 2
+  multiples_of_five = 199 * (5 + 995) / 2
+  multiples_of_fifteen = 66 * (15 + 990) / 2
+  puts multiples_of_three + multiples_of_five - multiples_of_fifteen
+end
+
+def benchmark(m)
+  start = Time.now
+  self.send(m)
+  finish = Time.now
+  puts "call to '#{m}' took #{(finish - start) * 1000}ms"
+end
+
+benchmark :first_attempt
+benchmark :optimized
